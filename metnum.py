dd073ddaed6bb841f40105f31267e57868c83237
gzadkiel <62406795+gzadkiel@users.noreply.github.com>
2020-04-24 17:44:12 -0300

    added fixedpoint method

--- metnum.py
+++ metnum.py
@@ -5,7 +5,12 @@
 import numexpr as ne
 from sympy import * 
 
+n = 0
+ex = ey = 9999
+c = 0
+
 def bisection(f,a,b,tx,ty):
+	global n, c, ex, ey
 	
 	def fun(x):
 		return ne.evaluate(f) 
@@ -29,6 +34,7 @@ def bisection(f,a,b,tx,ty):
 	return print(n, c, fun(c), ex, ey)
 
 def regulafalsi(f,a,b,tx,ty): 
+	global n, c, ex, ey
 	
 	def fun(x): 
 		return ne.evaluate(f)
@@ -49,8 +55,42 @@ def regulafalsi(f,a,b,tx,ty):
 	else: 
 		print('no root found on [',a,' ',b,']')
 	
-	return print(n, c, func(c), ex, ey) 
-  
-  if __name__ == "__main__":
+	return print(n, c, fun(c), ex, ey) 
+
+def fixedpoint(f,g,a,tx,ty):
+	#import numpy as np 
+	global n, c, ex, ey
+	
+	def fun(x):
+		return ne.evaluate(f) 
+	def gun(x):
+		return ne.evaluate(g)
+	x = var('x')
+	gd = sympify(g) 
+	gp = gd.diff(x)
+	gstr = str(gp)
+	def gup(x):
+		return ne.evaluate(gstr) 
+	cond = gup(a)  
+	
+	if abs(cond) < 1:
+		n = 0
+		c = a
+		ey = ex = 9999
+		while ex > tx and ey > ty:
+			n = n+1
+			cp = c
+			c = gun(cp) 
+			ex = abs(c-cp)
+			ey = abs(fun(c))
+	else: 
+		print('please input another x = g(x) function') 
+	
+	return print(n, c, fun(c), ex, ey)
+
+
+
+if __name__ == "__main__":
 	bisection() 
 	regulafalsi()
+	fixedpoint()
